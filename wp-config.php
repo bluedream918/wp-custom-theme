8260652eb0025fe818fa308070d44b7f5bc04f75
bluedream918 <228267049+bluedream918@users.noreply.github.com>
2025-11-02 01:44:09 -0400

    feat: add about html & configure page

--- wp-config.php
+++ wp-config.php
@@ -2,52 +2,34 @@
 /**
  * The base configuration for WordPress
  *
- * The wp-config.php creation script uses this file during the installation.
- * You don't have to use the web site, you can copy this file to "wp-config.php"
- * and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * Database settings
- * * Secret keys
- * * Database table prefix
- * * Localized language
- * * ABSPATH
- *
  * @link https://wordpress.org/support/article/editing-wp-config-php/
- *
  * @package WordPress
  */
 
-// ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
+// ** Database settings - from LocalWP ** //
+define( 'DB_NAME',     'local' );   // LocalWP default (change if yours differs)
+define( 'DB_USER',     'root' );    // LocalWP default
+define( 'DB_PASSWORD', 'root' );    // LocalWP default
 
-/** Database username */
-define( 'DB_USER', 'root' );
+/**
+ * DB host:
+ * - Use the Local socket for web requests ('localhost')
+ * - Use TCP with explicit port for WP-CLI (e.g., 127.0.0.1:10005)
+ *   -> Update 10005 to match your Local site's DB port.
+ */
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
+	define( 'DB_HOST', 'localhost' ); // <-- change 10005 to your Local DB port
+} else {
+	define( 'DB_HOST', 'localhost' );
+}
 
-/** Database password */
-define( 'DB_PASSWORD', 'root' );
-
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
-
-/** Database charset to use in creating database tables. */
+/** Database charset/collation */
 define( 'DB_CHARSET', 'utf8' );
-
-/** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
 /**#@+
  * Authentication unique keys and salts.
- *
- * Change these to different unique phrases! You can generate these using
- * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
- *
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
- *
- * @since 2.6.0
+ * Change these to different unique phrases!
  */
 define( 'AUTH_KEY',          'cV?C25?hkDuj2A3?s2groaT{6}~xw$6XA~-{T0_cNw*vLj;CDc!^,hmkJizk`&Sn' );
 define( 'SECURE_AUTH_KEY',   'w$7+]f3+1&,3zSyrfdpQ%&!Cc~PzkJ~!3$)P?zyU.&.c7B*_{gYh{4VC2C  Ey3P' );
@@ -58,40 +40,22 @@ define( 'SECURE_AUTH_SALT',  '9!8-tU(G+;OZ#MNXInfgZm_v Q]IiqQ?lE@W9^[(fiR01PsCU!
 define( 'LOGGED_IN_SALT',    'J&1MuNE[I?_y`2{$Ay+${NdE`OqHM]$hVGGi=6as]uF?eWu/tjno[2) cJlMi0/E' );
 define( 'NONCE_SALT',        'fof+@aEJ-{72<Nr-`Lu*g?LWY]Cr fX-3^N|Dd+W&$hE:BpVivVR<xwT5Zfz$,pF' );
 define( 'WP_CACHE_KEY_SALT', 'Nuh)Gs%h8SKM|f{57j%1g:}}wqpzj6%7P=RVxu{F<]bz9sdRc- Lzf_]udCmK#hQ' );
-
-
 /**#@-*/
 
 /**
  * WordPress database table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
  */
 $table_prefix = 'wp_';
 
-
 /* Add any custom values between this line and the "stop editing" line. */
 
-
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://wordpress.org/support/article/debugging-in-wordpress/
- */
+/** For developers: WordPress debugging mode. */
 if ( ! defined( 'WP_DEBUG' ) ) {
 	define( 'WP_DEBUG', false );
 }
 
 define( 'WP_ENVIRONMENT_TYPE', 'local' );
+
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
---
+++ wp-content/themes/twentytwentyfour/page-about.php
@@ -0,0 +1,6 @@
+<?php /* Template Name: About Page */ get_header(); ?>
+<main class="container">
+  <h1><?php the_title(); ?></h1>
+  <section><?php the_content(); ?></section>
+</main>
+<?php get_footer(); ?>
